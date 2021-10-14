d5f7cb4f235f51dae84b191f5d878d8865529908
Denperidge <denperidge@gmail.com>
2021-10-14 21:59:46 +0200

    Renamed project, slight Google API progress

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 
 
 https://github.com/googleapis/google-api-java-client/
+
+https://developers.google.com/sheets/api/quickstart/java#further_reading
--- app/src/main/java/com/denperidge/spreadsheetsms/MainActivity.java
+++ app/src/main/java/com/denperidge/spreadsheetsms/MainActivity.java
@@ -7,14 +7,40 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.time.temporal.ValueRange;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    NetHttpTransport http_transport;
+    JsonFactory json_factory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            http_transport = GoogleNetHttpTransport.newTrustedTransport();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        json_factory = new GsonFactory();
+
 
         EditText etURL = findViewById(R.id.etURL);
         etURL.addTextChangedListener(new android.text.TextWatcher() {
@@ -28,13 +54,27 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                getDataFromGSheet(s.toString());
+                try {
+                    getDataFromGSheet(s.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
     }
 
-    private String getDataFromGSheet(String url) {
-    
+    private String getDataFromGSheet(String url) throws IOException {
+        String spreadsheetId = url.split("/")[4];
+        HttpRequestInitializer httpRequestInitializer = new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest request) throws IOException {
+
+            }
+        };
+        Sheets service = new Sheets.Builder(http_transport, json_factory, httpRequestInitializer).build();
+        com.google.api.services.sheets.v4.model.ValueRange response =
+                service.spreadsheets().values().get(spreadsheetId, "A:Z").execute();
 
 
         return url;
